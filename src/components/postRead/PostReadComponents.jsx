eb0df19a87702c9b26c73a3f3b9e458c44f80218
donguk <sksmstek7@naver.com>
2025-08-18 21:57:35 +0900

    FEAT : 로그인에 따른 댓글창 활성화 기능 추가

--- src/components/postRead/PostReadComponents.jsx
+++ src/components/postRead/PostReadComponents.jsx
@@ -8,135 +8,79 @@ import line4 from "./img/vertical_bar.svg";
 import styles from "./PostReadComponents.module.css";
 import vector1 from "./img/recomment.svg";
 import { useSearchParams } from "react-router-dom";
-import { getPostData } from "../../api/postApi";
+import { getPostData, getCommentData } from "../../api/postApi";
+import { useSelector } from "react-redux";
 
 export const PostReadComponents = () => {
-
+  // 게시글 번호 추출용
   const [searchParams, setSearchParams] = useSearchParams();
-  const boardId = Number(searchParams.get("boardId" || 0));
+  const boardId = Number(searchParams.get("boardId") || 0);
+  // 콘텐츠, 댓글정보 가져오기용
   const [contentData, setContentData] = useState(null);
+  const [commentData, setCommentData] = useState(null);
+  // 댓글 활성화 여부 결정을 위한 jwt토큰 추출용
+  const hasJwt = useSelector((store) => Boolean(store.auth.hwanuAccessToken));
+  // console.log("hasJwt : ", hasJwt);
+  // 댓글내용
+  const [commentText, setCommentText] = useState("");
+  console.log("작성중인 댓글내용 : ", commentText);
 
-    // 게시글 가져오는 함수
+  // 게시글 가져오는 함수
   const fetchPostData = async () => {
     const response = await getPostData(boardId);
     setContentData(response);
-    console.log("ContentData 완료:", response);
+    // console.log("ContentData 완료:", response);
   };
 
-    // 마운트 시 1번만 실행
+  // 게시글 가져오는 함수
+  const fetchCommentData = async () => {
+    const response = await getCommentData(boardId);
+    setCommentData(response);
+    // console.log("CommentData 완료:", response);
+    // console.log("Comment 수량 ", response.length);
+  };
+
+  // 마운트 시 1번만 실행
   useEffect(() => {
     if (boardId) {
+      fetchCommentData();
       fetchPostData();
     }
   }, []);
 
-  if (!contentData) { return null}
+  if (!contentData || !commentData) {
+    return null;
+  }
 
   return (
     // <div className={styles.board_container}></div>
     <div className={styles.frame}>
       <div className={styles.div}>
         <div className={styles["div-2"]}>
-          <div className={styles["div-3"]}>
-            <div className={styles["text-wrapper-2"]}>{contentData.title}</div>
-            <div className={styles["div-4"]}>
-              <div className={styles.view}>
-                <div className={styles["text-wrapper-3"]}>작성자 |</div>
-                <div className={styles["div-wrapper"]}>
-                  <div className={styles["text-wrapper-4"]}>{contentData.nickname}</div>
-                </div>
-              </div>
-              <div className={styles.view}>
-                <div className={styles["text-wrapper-3"]}>조회수 |</div>
-                <div className={styles["div-wrapper"]}>
-                  <div className={styles["text-wrapper-4"]}>{contentData.viewCnt}</div>
-                </div>
-              </div>
-              <div className={styles.view}>
-                <div className={styles["text-wrapper-3"]}>작성일 |</div>
-                <div className={styles["div-wrapper"]}>
-                  <div className={styles["text-wrapper-4"]}>
-                    {contentData.createdAt === contentData.updatedAt ? contentData.updatedAt : contentData.updatedAt+" (수정됨)"}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["view-2"]}>
-            <img className={styles.line} alt="Line" src={line1} />
-            <div className={styles["view-3"]}>
-              <p className={styles.p}>
-                {contentData.content}
-              </p>
-            </div>
-            <img className={styles.img} alt="Line" src={line2} />
-          </div>
-
+          <Content contentData={contentData} />
           <div className={styles["div-5"]}>
             <div className={styles["view-4"]}>
               <div className={styles["text-wrapper-5"]}>댓글</div>
               <div className={styles["div-wrapper-2"]}>
-                <div className={styles["text-wrapper-3"]}>[00]</div>
+                <div className={styles["text-wrapper-3"]}>
+                  [{commentData.length}]
+                </div>
               </div>
             </div>
 
             <div className={styles["view-5"]}>
-              <div className={styles["view-6"]}>
-                <div className={styles["div-6"]}>
-                  <div className={styles["text-wrapper-6"]}>강아지 사랑</div>
-                  <div className={styles["text-wrapper-7"]}>대댓글</div>
-                  <img className={styles["line-2"]} alt="Line" src={line42} />
-                  <div className={styles["text-wrapper-8"]}>수정</div>
-                  <img className={styles["line-2"]} alt="Line" src={image} />
-                  <div className={styles["text-wrapper-8"]}>삭제</div>
-                </div>
-                <div className={styles["div-wrapper-3"]}>
-                  <p className={styles["text-wrapper-9"]}>
-                    에이 그럴리가요 !? <br />
-                    말도 안되는 소리 하지마세요 ㅋㅋ
-                  </p>
-                </div>
-              </div>
-
-              <div className={styles["frame-wrapper"]}>
-                <div className={styles["div-7"]}>
-                  <div className={styles["div-8"]}>
-                    <img className={styles.vector} alt="Vector" src={vector1} />
-                    <div className={styles["div-9"]}>
-                      <div className={styles["text-wrapper-10"]}>
-                        강아지 팩폭러
-                      </div>
-                      <div className={styles["text-wrapper-7"]}>대댓글</div>
-                      <img className={styles["line-2"]} alt="Line" src={line4} />
-                      <div className={styles["text-wrapper-8"]}>수정</div>
-                      <img className={styles["line-2"]} alt="Line" src={line3} />
-                      <div className={styles["text-wrapper-8"]}>삭제</div>
-                    </div>
-                  </div>
-                  <div className={styles["div-wrapper-4"]}>
-                    <p className={styles["text-wrapper-9"]}>
-                      그런 개들 있음 ㅋㅋ <br />
-                      강아지가 그렇게 좋으세용?
-                    </p>
-                  </div>
-                </div>
-              </div>
+              {/* 댓글 ------------------------------ */}
+              {commentData.map((comment) => (
+                <React.Fragment key={comment.commentId}>
+                  <Comment comment={comment} /> {/* JSX 컴포넌트 반환 */}
+                  <ReComment recomments={comment.recomment} />
+                </React.Fragment>
+              ))}
+              {/* 댓글 ------------------------------ */}
             </div>
           </div>
 
-          <div className={styles["overlap-group-wrapper"]}>
-            <div className={styles["overlap-group"]}>
-              <div className={styles["div-wrapper-5"]}>
-                <p className={styles["text-wrapper-11"]}>
-                  댓글은 로그인이 필요한 서비스 입니다.
-                </p>
-              </div>
-              <div className={styles["div-wrapper-6"]}>
-                <div className={styles["text-wrapper-12"]}>댓글쓰기</div>
-              </div>
-            </div>
-          </div>
+          <NewComment {...{hasJwt, commentText, setCommentText}}></NewComment>
 
           <div className={styles["div-10"]}>
             <div className={styles["div-wrapper-7"]}>
@@ -152,3 +96,126 @@ export const PostReadComponents = () => {
   );
 };
 export default PostReadComponents;
+
+function Content({ contentData }) {
+  return (
+    <>
+      <div className={styles["div-3"]}>
+        <div className={styles["text-wrapper-2"]}>{contentData.title}</div>
+        <div className={styles["div-4"]}>
+          <div className={styles.view}>
+            <div className={styles["text-wrapper-3"]}>작성자 |</div>
+            <div className={styles["div-wrapper"]}>
+              <div className={styles["text-wrapper-4"]}>
+                {contentData.nickname}
+              </div>
+            </div>
+          </div>
+          <div className={styles.view}>
+            <div className={styles["text-wrapper-3"]}>조회수 |</div>
+            <div className={styles["div-wrapper"]}>
+              <div className={styles["text-wrapper-4"]}>
+                {contentData.viewCnt}
+              </div>
+            </div>
+          </div>
+          <div className={styles.view}>
+            <div className={styles["text-wrapper-3"]}>작성일 |</div>
+            <div className={styles["div-wrapper"]}>
+              <div className={styles["text-wrapper-4"]}>
+                {contentData.createdAt === contentData.updatedAt
+                  ? contentData.updatedAt
+                  : contentData.updatedAt + " (수정됨)"}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["view-2"]}>
+        <img className={styles.line} alt="Line" src={line1} />
+        <div className={styles["view-3"]}>
+          <p className={styles.p}>{contentData.content}</p>
+        </div>
+        <img className={styles.img} alt="Line" src={line2} />
+      </div>
+    </>
+  );
+}
+
+function Comment({ comment }) {
+  return (
+    <div className={styles["view-6"]}>
+      <div className={styles["div-6"]}>
+        <div className={styles["text-wrapper-6"]}>{comment.nickname}</div>
+        <div className={styles["text-wrapper-7"]}>대댓글</div>
+        <img className={styles["line-2"]} alt="Line" src={line42} />
+        <div className={styles["text-wrapper-8"]}>수정</div>
+        <img className={styles["line-2"]} alt="Line" src={image} />
+        <div className={styles["text-wrapper-8"]}>삭제</div>
+      </div>
+      <div className={styles["div-wrapper-3"]}>
+        <p className={styles["text-wrapper-9"]}>{comment.content}</p>
+      </div>
+    </div>
+  );
+}
+
+function ReComment({ recomments }) {
+  // console.log("recomment :: ", recomments);
+  return (
+    <>
+      {recomments.map((recomment) => (
+        // {console.log("recomment :::",recomment)}
+        <React.Fragment key={recomment.commentId}>
+        <div className={styles["frame-wrapper"]}>
+          <div className={styles["div-7"]}>
+            <div className={styles["div-8"]}>
+              <img className={styles.vector} alt="Vector" src={vector1} />
+              <div className={styles["div-9"]}>
+                <div className={styles["text-wrapper-10"]}>
+                  {recomment.nickname}
+                </div>
+                <div className={styles["text-wrapper-7"]}>대댓글</div>
+                <img className={styles["line-2"]} alt="Line" src={line4} />
+                <div className={styles["text-wrapper-8"]}>수정</div>
+                <img className={styles["line-2"]} alt="Line" src={line3} />
+                <div className={styles["text-wrapper-8"]}>삭제</div>
+              </div>
+            </div>
+            <div className={styles["div-wrapper-4"]}>
+              <p className={styles["text-wrapper-9"]}>{recomment.content}</p>
+            </div>
+          </div>
+        </div>
+        </React.Fragment>
+      ))}
+    </>
+  );
+}
+
+function NewComment({ hasJwt, commentText, setCommentText}) {
+  return (
+    <div className={styles["overlap-group-wrapper"]}>
+      <div className={styles["overlap-group"]}>
+        <div className={styles["div-wrapper-5"]}>
+          {hasJwt ? (
+            <textarea
+              className={styles["text-wrapper-11"]}
+              placeholder="댓글을 남겨주세요."
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+            ></textarea>
+          ) : (
+            <p className={styles["text-wrapper-11"]}>
+              댓글은 로그인이 필요한 서비스 입니다.
+            </p>
+          )}
+        </div>
+        <div className={styles["div-wrapper-6"]}>
+          <div className={styles["text-wrapper-12"]}>댓글쓰기</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,            // 0.0.0.0으로 바꿔서 외부에서 접근 가능하게 함
-//     port: 5173,            // 기본 포트 설정
-//     strictPort: true,      // 포트 고정 (이미 점유 중이면 오류 발생)
-//     cors: true             // 모든 origin 허용 (Nginx 프록시용)
-//   }
-// })
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,           // 0.0.0.0
-    port: 5173,
-    strictPort: true,
-    cors: true,
-    // ⬇️ 여기 추가
-    allowedHosts: ['hwanu.site', 'www.hwanu.site'],
-    // HMR가 새로고침만 되고 안 붙으면 아래 3줄도 같이 켜줘
-    hmr: {
-      host: 'hwanu.site',
-      protocol: 'wss',
-      clientPort: 443
-    }
+    host: true,            // 0.0.0.0으로 바꿔서 외부에서 접근 가능하게 함
+    port: 5173,            // 기본 포트 설정
+    strictPort: true,      // 포트 고정 (이미 점유 중이면 오류 발생)
+    cors: true             // 모든 origin 허용 (Nginx 프록시용)
   }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,           // 0.0.0.0
+//     port: 5173,
+//     strictPort: true,
+//     cors: true,
+
+//     allowedHosts: ['hwanu.site', 'www.hwanu.site'],
+//     // 지정된 도메인만 서버에 접근할 수 있도록 제한
+//     // 보안을 위해 의도하지 않은 도메인에서의 접근을 차단
+
+//     // HMR가 새로고침만 되고 안 붙으면 아래 3줄도 같이 켜줘
+//     hmr: {
+//       host: 'hwanu.site',
+//       protocol: 'wss',
+//       clientPort: 443
+//     }
+//   }
+// })
